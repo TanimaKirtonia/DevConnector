215d3cc44a06054bdf85cf9d0bf8430a62626844
Tanima Kirtonia <tanima0599@gmail.com>
2021-01-09 17:51:13 +0530

    jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
  const express = require('express');
  const router = express.Router();
+ const gravatar = require('gravatar');
+ const bcrypt = require('bcryptjs');
+ const jwt = require('jsonwebtoken');
+ const config = require('config');
+
  const {check, validationResult} = require('express-validator');
+ const User = require('../../models/User');
 
  // @route   POST api/users
  // @desc    Register User
@@ -12,12 +18,63 @@
         min:8
     })
  ],
- (req, res) => {
-     const errors = validationResult(req);
-     if(!errors.isEmpty()){
-         return res.status(400).json({ errors: errors.array()});
-     }
-     res.send('User route')
-    });
+    async (req, res) => {
+         const errors = validationResult(req);
+         if (!errors.isEmpty()) {
+             return res.status(400).json({ errors: errors.array() });
+         }
+
+         // req.body.fieldname to get data from postman 
+         const { name, email, password } = req.body;
+         try {
+             //see if the user exists 
+             let user = await User.findOne({ email });
+             if (user) {
+                 return res.status(400).json({ errors: [{ msg: 'user already exists' }] });
+             }
+
+             //get user's gravatar
+             const avatar = gravatar.url(email, {
+                 s: '200',
+                 r: 'pg',
+                 d: 'mm' //default image 
+             });
+             user = new User({
+                 name,
+                 email,
+                 avatar,
+                 password
+             });
+
+             // encrypt the password 
+             const salt = await bcrypt.genSalt(10); //salt 
+             user.password = await bcrypt.hash(password, salt); //hashing
+
+             await user.save(); //anything that returns a promise should have "await" in front of the statement 
+            //return jsonwebtoken JWT token to authenticate and acess protected routes
+             const payload = {
+                 user: {
+                     id: user.id
+                 }
+             }
+             
+             jwt.sign(
+                 payload, 
+                 config.get('jwtSecret'),
+                 { expiresIn: 3600000 }, (err, token)=>{
+                     if (err) throw err;
+                     res.json({ token });
+                 }
+                 );
+
+         }
+         catch (err) {
+             console.error(err.message);
+             res.status(500).send('Server Error');
+         }
+
+
+
+     });
 
  module.exports = router;
