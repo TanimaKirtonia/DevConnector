ba295be79cd4915ec0c4e59fe563ae4466ccac3a
Tanima Kirtonia <tanima0599@gmail.com>
2021-01-09 14:28:58 +0530

    Intial commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+//mongo db connection 
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async function () {
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Mongo DB Connected..');
+    }
+    catch (err) {
+        console.log(err.message);
+        //exit process with failure
+        process.exit(1);
+    }
+
+};
+module.exports = connectDB;
