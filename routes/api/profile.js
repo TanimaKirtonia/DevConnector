7eef1dbab0e3b66edaeffb4f1994ef4940a6e93e
Tanima Kirtonia <tanima0599@gmail.com>
2021-01-14 00:26:08 +0530

    user profile creation and updation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,10 +40,78 @@ router.post(
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-   
-      // your comments
+      
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body; 
+      
+    //build profile object 
+    const profileFields = {};
+    profileFields.user =req.user.id; 
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername =githubusername; 
+    //skills ka array 
+    if (skills){
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
     }
-  );
+
+    //BUILD SOCIAL OBJECT
+    profileFields.social = {} // part of "social" array 
+      if (youtube) profileFields.social.youtube = youtube;
+      if (twitter) profileFields.social.twitter = twitter;
+      if (facebook) profileFields.social.facebook = facebook;
+      if (linkedin) profileFields.social.linkedin = linkedin;
+      if (instagram) profileFields.social.instagram = instagram;
+      
+      
+    try{
+      let profile = await Profile.findOne({
+        user: req.user.id
+      });
+      if(profile){
+          //update profile 
+          profile = await Profile.findOneAndUpdate(
+          { 
+            user: req.user.id
+          },
+          {
+            $set: profileFields
+          },
+          {
+            new: true
+          }
+          );
+
+          return res.json(profile);
+      }
+
+      //create profile 
+
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    }
+    catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+    } //async function close 
+  ); //post function close 
 
 
 module.exports = router;
