80ed462b4acf1509c738194ac1e985b52fe92f95
Tanima Kirtonia <tanima0599@gmail.com>
2021-01-25 20:11:48 +0530

    Github Repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const request = require('request');
+const config = require('config');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile 
@@ -331,11 +333,41 @@ router.post(
 
       res.json(profile);
      }
-     catch (err) {
+     catch(err){
       console.error(err.message);
-      return res.status(400).json({ msg: 'Profile not found' });
+      res.status(500).send('Server Error');
     }
   
+  });
+
+  // @route    GET api/profile/github/:username
+  // @desc     Get user repos from Github
+  // @access   Public
+
+  router.get('/github/:username', (req,res)=> {
+    try{
+      const options = {
+        uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}`, //backticks ka key below escape key 
+        method: 'GET',
+        headers: {
+          'user-agent': 'node.js'
+        }
+      };
+
+      request(options, (error, response, body)=> {
+        if(error) console.error(error);
+
+        if(response.statusCode!==200){
+         return res.status(404).json({ msg: 'No Github Profile found'});
+        }
+
+        res.json(JSON.parse(body));
+      });
+    }
+    catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   });
 
 module.exports = router;
