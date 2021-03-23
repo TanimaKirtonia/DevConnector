2f42fe864e724d16e455533728b87ff5e7fe5827
Tanima Kirtonia <tanima0599@gmail.com>
2021-03-23 19:19:22 +0530

    dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 //redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -31,6 +32,7 @@ const App = () => {
         <Switch>
           <Route exact path="/register" component={Register}/>
           <Route exact path="/login" component={Login}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
         </Switch>
       </section>
     </Fragment>
